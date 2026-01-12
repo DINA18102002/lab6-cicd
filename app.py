79d839b44ae8c0c767ee9b120754c70dbd7d2e56
DINA18102002 <theenaxz@gmail.com>
2026-01-12 07:15:08 +0000

    fin

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi - LAB_6 CI/CD PIPELINE 😊 V2 CONFIRMED"
+    return "Hi ---> CI/CD PIPELINE 😊 V2 CONFIRMED"
 
 app.run(host="0.0.0.0", port=8080)
